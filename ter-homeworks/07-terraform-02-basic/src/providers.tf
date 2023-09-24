6baa1284e8d78a212fd6e152fd18946cf1afb434
Nikita Bulgakov <nbulgakov89@gmail.com>
2023-09-24 17:16:17 +0300

    [07-terraform-02-base] final (with fix url)
    Signed-off-by: Nikita Bulgakov <nbulgakov89@gmail.com>

--- ter-homeworks/07-terraform-02-basic/src/providers.tf
+++ ter-homeworks/07-terraform-02-basic/src/providers.tf
@@ -7,8 +7,10 @@ terraform {
   required_version = ">=0.13"
 }
 
+## вместо ключа ssh использую ключ доступа в файле
+
 provider "yandex" {
-  service_account_key_file = "key.json"
+  service_account_key_file = "~/secret/yc-admin-key.json"
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.default_zone
